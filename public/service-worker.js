3f7f384785b2c92292a3139f8d5b5033179bcc75
umairdev58 <umair.dev58@gmail.com>
2026-06-15 12:39:28 +0500

    added service worker

---
+++ public/service-worker.js
@@ -0,0 +1,20 @@
+// public/service-worker.js
+
+const CACHE_NAME = 'sijil-cache-v1';
+const urlsToCache = ['/', '/index.html'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
